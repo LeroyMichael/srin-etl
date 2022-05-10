d5707c4ee9a1f76ab8018c357c2d50e2ce450722
Your Name <lrymchl@gmail.com>
2022-05-11 00:05:49 +0700

    fixing download

--- app.py
+++ app.py
@@ -2,7 +2,8 @@
 from flask import Flask, jsonify, Response
 from flask_restful import Resource, Api
 from flask_cors import CORS
-from petl import fromcsv, look, join, tocsv
+from petl import fromcsv, look, join, tocsv, todataframe
+import pandas as pd
 
 # Inisiation object Flask
 app = Flask(__name__)
@@ -13,29 +14,28 @@ api = Api(app)
 # Inisiation object CORS
 CORS(app)
 
-
-
 # Create class for resource
 class Etl(Resource):
         # Get method
         def post(self):
             # Extract
-            customer = fromcsv('/assets/Customer_ID_Superstore.csv')
-            product = fromcsv('/assets/Product_ID_Superstore.csv')
-            final = fromcsv('/assets/final_superstore.csv')
+            customer = fromcsv('assets/Customer_ID_Superstore.csv')
+            product = fromcsv('assets/Product_ID_Superstore.csv')
+            final = fromcsv('assets/final_superstore.csv')
 
-            # Join table
+            # Transform
             merge = join(final, customer, key='customer_id')
             merge = join(merge, product, key='product_id')
-            
+            df = todataframe(merge)
+            # tocsv(merge, 'output.csv')
+
             # Load to csv
-            tocsv(merge, 'output.csv')
 
-            with open('output.csv') as output:
-                response = Response(output, mimetype='text/csv')
-                response.headers['Content-Disposition'] = u'attachment; filename=output.csv'
-
+            response = Response(df.to_csv())
+            response.headers['Content-Disposition'] = u'attachment; filename=output.csv'
             return response
+
+            
 
 # Resource setup
 api.add_resource(Etl, '/etl', methods=["post"])
